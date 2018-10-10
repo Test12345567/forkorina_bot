fd53441885f9330ecb3d7d9ca2655baf90165163
Jardev <vdemchenko99@gmail.com>
2018-10-10 15:58:21 +0300

    bot 0.1.0

---
+++ app.js
@@ -0,0 +1,28 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '613248910:AAFxTOijuGVR49p-rHM8U2yEwElUeJjsXcE';
+
+const bot = new TelegramBot(token, {polling: true});
+
+let list = [];
+
+bot.on('message', function (msg) {
+
+    const userId = msg.from.id;
+
+    list.push({ 'uid': userId});
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+function messageForKorina() {
+    const hours = new Date().getHours();
+    if(hours > 1 && hours < 6)
+        return;
+
+    for(let i = 0; i < list.length; i++){
+        bot.sendMessage(list[i].uid, 'Учи JS блять!');
+    }
+}
+
+setInterval(messageForKorina, 2*60*60*1000);
